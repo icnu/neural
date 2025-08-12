dfca7312359829842fe362ddcc3097a17767bc8a
icnu <172075965+icnu@users.noreply.github.com>
2025-08-12 14:58:40 +0530

    add acl in token index canister

---
+++ src/token_index_canister/src/guard.rs
@@ -0,0 +1,21 @@
+use crate::access::{is_authorized, AccessMask};
+
+pub fn guard_caller_is_controller() -> Result<(), String> {
+    let caller = ic_cdk::api::msg_caller();
+    
+    if !ic_cdk::api::is_controller(&caller) {
+        Err("Caller not a controller, unauthorized".to_string())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn guard_caller_is_feeder() -> Result<(), String> {
+    let caller = ic_cdk::api::msg_caller();
+    
+    if !is_authorized(caller, AccessMask::Feeder) {
+        Err("Caller is unauthorized".to_string())
+    } else {
+        Ok(())
+    }
+}
--- src/token_index_canister/src/lib.rs
+++ src/token_index_canister/src/lib.rs
@@ -1,6 +1,19 @@
-use ic_cdk::export_candid;
+use candid::{Principal};
+use ic_cdk::{export_candid, query, update};
+use crate::{access::{is_authorized, set_is_authorized, AccessMask}, guard::guard_caller_is_controller};
 
 mod access;
 mod memory;
+mod guard;
+
+#[update(guard = "guard_caller_is_controller")]
+fn add_feeder_agent(principal: Principal) {
+    set_is_authorized(principal, AccessMask::Feeder);
+}
+
+#[query]
+fn is_feeder_agent(principal: Principal) -> bool {
+    is_authorized(principal, AccessMask::Feeder)
+}
 
 export_candid!();
